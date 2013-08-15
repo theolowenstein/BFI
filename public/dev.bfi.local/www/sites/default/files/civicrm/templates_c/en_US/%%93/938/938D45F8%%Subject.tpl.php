48897c3fc35d4370ee39b15aa0bb88316f665947
tckdesign <toshiro.kida@gmail.com>
2013-08-14 22:29:57 -0400

    Removed all CiviCRM templates to work w/ .gitignore

--- public/dev.bfi.local/www/sites/default/files/civicrm/templates_c/en_US/%%93/938/938D45F8%%Subject.tpl.php
+++
@@ -1,4 +0,0 @@
-<?php /* Smarty version 2.6.27, created on 2013-08-14 16:13:00
-         compiled from CRM/Block/Subject.tpl */ ?>
-<span class='crm_blocktitle'><?php echo $this->_tpl_vars['subject']; ?>
-</span>
